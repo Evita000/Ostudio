152537829814b75bd3fefc2403bf911a50189ab9
Evita Ramotar <183926691+Evita000@users.noreply.github.com>
2025-10-01 15:58:36 -0400

    test.html

---
+++ .github/sketch.js
@@ -0,0 +1,16 @@
+let balloonGIT;
+
+function preload() {
+  balloonGIT = loadImage('balloon.gif');
+}
+
+function setup() {
+
+
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220,255,0);
+  image(balloon,0,0);
+}
